6adb7974377c21f9d2a55660af324d3c7f22e03b
Joud <joud.mahmoud1323@gmail.com>
2026-06-29 16:26:12 +0200

    upload entities

---
+++ OrderManagementSystem.Domain/Entities/Customer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagementSystem.Domain.Entities
+{
+    public class Customer:User
+    {
+        public virtual ICollection<Order> Orders { get; set; }
+    }
+}
---
+++ OrderManagementSystem.Domain/Entities/OrderItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagementSystem.Domain.Entities
+{
+    public class OrderItem:Base
+    {
+
+
+
+        [ForeignKey("Order")]
+        public string OrderId { get; set; }
+        public virtual Order Order { get; set; }
+    }
+}
